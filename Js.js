2b78ca9a35a3e0620d884e299a42e820fac6ca70
Umbro9 <lolxdlolfabek@gmail.com>
2026-03-06 23:41:18 +0100

    asd

--- Js.js
+++ Js.js
@@ -36,3 +36,43 @@ document.addEventListener("DOMContentLoaded", function(){
 
     
 });
+
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for(let i=0;i<80;i++){
+  particles.push({
+    x: Math.random()*canvas.width,
+    y: Math.random()*canvas.height,
+    speed: 0.2 + Math.random()*1.5,
+    size: Math.random()*4+2,
+    alpha: Math.random()
+  });
+}
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  particles.forEach(p=>{
+    p.y += p.speed;
+
+    if(p.y > canvas.height){
+      p.y = -10;
+      p.x = Math.random()*canvas.width;
+    }
+
+    ctx.beginPath();
+    ctx.arc(p.x,p.y,p.size,0,Math.PI*2);
+    ctx.fillStyle = `rgba(255,255,255,${p.alpha})`;
+    ctx.fill();
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
